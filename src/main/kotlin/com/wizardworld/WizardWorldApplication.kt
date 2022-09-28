9598c4876de7ffddc2214d2a4e0117269001b553
MARCELA LOPES <you@example.com>
2022-09-28 08:48:27 -0300

    initial api

---
+++ src/main/kotlin/com/wizardworld/WizardWorldApplication.kt
@@ -0,0 +1,11 @@
+package com.wizardworld
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WizardWorldApplication
+
+fun main(args: Array<String>) {
+	runApplication<WizardWorldApplication>(*args)
+}
---
+++ src/test/kotlin/com/wizardworld/WizardWorldApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wizardworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WizardWorldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
